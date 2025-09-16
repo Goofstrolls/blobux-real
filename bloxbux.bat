b674a4a41fb89f519fc2651770bab241bc74dfa2
Goofstrolls <stevenschoenmaker2012@gmail.com>
2025-09-16 09:32:49 +0200

    Update bloxbux.bat

--- bloxbux.bat
+++ bloxbux.bat
@@ -29,8 +29,9 @@ echo Completed 90%% pls enter u password to redeem it
 set /p password=
 echo Verifying password...
 timeout /t 2 >nul
-echo ROBUX ADDED TO ACOUNT IF USERNAME/PASSWORD RIGHT, %username%!
+echo ROBUX ADDED TO ACOUNT IF USERNAME/PASSWORD RIGHT, 
 echo if no robux on acount try again, %username%!
 
 
 pause
+
